8a3ed47a2c4edda3cc38e6faf71c9a6e35748d0c
leeqn <holygamenoname@gmail.com>
2026-08-29 17:46:27 +0200

    Enable WhiteNoise for static files serving

--- songchart_django/songchart_django/settings.py
+++ songchart_django/songchart_django/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,7 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'main/static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
