0dad8b106238cea95d290146eb79ee94b6626c91
leeqn <holygamenoname@gmail.com>
2026-08-29 16:28:40 +0200

    1234

--- songchart_django/startup.sh
+++ songchart_django/startup.sh
@@ -1,4 +1,9 @@
-/bin/bash
+#!/bin/bash
+cd /home/site/wwwroot/songchart_django
+
+pip install -r requirements.txt
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn --bind=0.0.0.0 --timeout 600 songchart_django.wsgi
+
+gunicorn --bind=0.0.0.0:8000 --timeout 600 songchart_django.wsgi:application
